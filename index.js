68fe1340e1d63ef6edabdd318e2e474d75cc65f2
mwong068 <mwong068@gmail.com>
2019-12-15 22:38:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+// document.addEventListener('DOMContentLoaded', (event) => {
+    function submitData (name, email) {
+    
+    let dataForSubmitting = {
+        name,
+        email
+    };
+    
+    let configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(dataForSubmitting)
+    };
+
+    return fetch('http://localhost:3000/users', configObj)
+    // Call the fetch function passing the url of the API as a parameter
+    .then(function(response) {
+        return response.json();
+    // Your code for handling the data you get from the API
+    })
+    .then(function(object){
+        document.body.innerHTML = object["id"];
+    })
+    .catch(function(error) {
+        document.body.innerHTML = "Unauthorized Access";
+//     // This is where you run code if the server returns any errors
+    });
+
+    };
+// });
